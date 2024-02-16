5fed603bcff2be054f84e06cc66512042197aa38
HerasimenkoAleksandr <a.gerasimenko.yuz@gmail.com>
2024-02-16 17:05:37 +0200

    3task

--- _layout.php
+++ _layout.php
@@ -8,31 +8,75 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" >
     <!--Let browser know website is optimized for mobile-->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <title>PHP SPD-111</title>
+    <title>PHP-DZ</title>
+	<link rel="stylesheet" href="/css/site.css" />
 </head>
 
 <body>
 <nav>
 	<div class="nav-wrapper deep-purple">
-	  <a href="/" class="brand-logo">PHP</a>
+	  <a href="/" class="brand-logo"><img src="/img/php.png"/></a>
 	  <ul id="nav-mobile" class="right ">
 		<?php foreach( [
 			'/basics' => 'Основи',
 			'/layout' => 'Шаблонізація', 
-            '/regexp' => 'Регулярні вирази' 
+            '/regexp' => 'Регулярні вирази' ,
+			'/api' => 'API',
+			'/registration' => 'Реєстрація'
 		] as $href => $name ) : ?>
 			<li <?= $uri==$href ? 'class="active"' : '' ?> ><a href="<?= $href ?>"><?= $name ?></a></li>
 		<?php endforeach ?>
+		<li><a class="modal-trigger" href="#modal-login">Вхід</a></li>
 	  </ul>
 	</div>
 </nav>
-  
+
 <div class="container">
-	<?php include $page_body ; ?>
+<div id="modal-login" class="modal">
+    <div class="modal-content">
+        <h4>Вхід</h4>
+        <form id="login-form">
+            <div class="input-field">
+                <input id="emailCheck" name="emailCheck" type="email" class="validate" required>
+                <label for="emailCheck">Email</label>
+            </div>
+            <div class="input-field">
+                <input id="passwordCheck" name="passwordCheck" type="password" class="validate" required>
+                <label for="passwordCheck">Пароль</label>
+            </div>
+            <button class="btn deep-purple" type="submit" name="action">Увійти</button>
+        </form>
+    </div>
+    <div class="modal-footer">
+        <a  href="#!" class="modal-close waves-effect waves-green btn-flat">Закрити</a>
+    </div>
 </div>
+	<?php include $page_body ; ?>
+	
+</div>
+
+
 
 <!-- Compiled and minified JavaScript -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<script src="/js/site.js"></script>
+
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+    var modalTrigger = document.querySelector('.modal-trigger');
+    var modalLogin = document.getElementById('modal-login');
+	var modalClose = document.querySelector('.modal-footer');
+
+    modalTrigger.addEventListener('click', function() {
+        modalLogin.style.display = 'block';
+    });
+
+	modalClose.addEventListener('click', function () {
+
+		 modalLogin.style.display = 'none';
+	})
+});
+</script>
 
 </body>
 </html>
---
+++ controllers/TestController.php
@@ -0,0 +1,90 @@
+<?php
+
+class TestController {
+	
+	public function serve() {
+		$method = strtolower( $_SERVER[ 'REQUEST_METHOD' ] ) ;   // метод запиту - GET, POST, ...
+		$action = "do_{$method}" ;
+		// чи визначений у даному об'єкті метод з іменем $action (do_get)
+		if( method_exists( $this, $action ) ) {
+			// якщо визначений, то викликаємо
+			$this->$action() ;   // у РНР $this-> обов'язково
+			// !! назву методу можна передати через змінну
+			// $this->$action()  == $this->do_get()
+		}
+		else {
+			http_response_code( 405 ) ;
+			echo "Method Not Allowed" ;
+		}
+	}
+	
+	protected function connect_db_or_exit() {
+		try {
+			return new PDO(
+				'mysql:host=localhost;dbname=php_dz;charset=utf8mb4', 
+				'php_dz_user', 'php_dz_pass', [
+					PDO::ATTR_PERSISTENT => true,
+					PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,					
+			] ) ;
+		} 
+		catch( PDOException $ex ) {
+			http_response_code( 500 ) ;
+			echo "Connection error: " . $ex->getMessage() ;
+			exit ;
+		}
+	}
+	
+	protected function do_get() {
+		$db = $this->connect_db_or_exit() ;
+		// виконання запитів
+		$sql = "CREATE TABLE  IF NOT EXISTS  Users (
+			`id`        CHAR(36)      PRIMARY KEY  DEFAULT ( UUID() ),
+			`email`     VARCHAR(128)  NOT NULL,
+			`name`      VARCHAR(64)   NOT NULL,
+			`password`  CHAR(32)      NOT NULL     COMMENT 'Hash of password',
+			`avatar`    VARCHAR(128)  NULL
+		) ENGINE = INNODB, DEFAULT CHARSET = utf8mb4";
+		try {
+			$db->query( $sql ) ;
+		}
+		catch( PDOException $ex ) {
+			http_response_code( 500 ) ;
+			echo "query error: " . $ex->getMessage() ;
+			exit ;
+		}
+		echo "Hello from do_get - Query OK" ;
+	}
+	
+	protected function do_post() {
+        $db = $this->connect_db_or_exit() ;
+		// виконання запитів
+		$sql = "CREATE TABLE  IF NOT EXISTS  User_log (
+			`id`           CHAR(36)      PRIMARY KEY  DEFAULT ( UUID() ),
+			`date-time`    DATETIME      NOT NULL,
+			`user-id`      CHAR(36)      NOT NULL,
+			`token`        CHAR(64)      NOT NULL     
+			
+		) ENGINE = INNODB, DEFAULT CHARSET = utf8mb4";
+		try {
+			$db->query( $sql ) ;
+		}
+		catch( PDOException $ex ) {
+			http_response_code( 500 ) ;
+			echo "query error: " . $ex->getMessage() ;
+			exit ;
+		}
+		echo "Hello from do_post - Query OK" ;
+		
+	}
+}
+/*
+Д.З. Створити форму реєстрації користувача
+з полями: email, name, password, repeat password, avatar (FILE)
+Реалізувати перехід на неї через <nav>
+
+На кнопку POST в тестовому API реалізувати створення 
+таблиці-журналу реєстрації ( id, date-time, user-id, token[Hash] )
+
+* Розібратись з модальними вікнами Materialize, реалізувати на
+їх основі діалог автентифікації (введеня email/password)
+*/
